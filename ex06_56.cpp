56f3e11828f86e90f612955d87bb6237116fed08
vovtshik <vovtshik@gmail.com>
2021-08-06 15:10:39 +0300

    Added implementation of functions tripleCallByValue and tripleByReference in file ex06_56.cpp

--- ex06_56.cpp
+++ ex06_56.cpp
@@ -5,11 +5,22 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-int tripleCallByValue(int)
+int tripleCallByValue(int);
 int tripleByReference(int&);
 
 int main()
 {
 
    return 0;
+}
+
+int tripleCallByValue(int count)
+{
+   return count * 3;
+
+}
+
+int tripleByReference(int& count)
+{
+   return count * 3;
 }
