6b0f494d899635d27e2bc0bdab0da19dab259743
vovtshik <vovtshik@gmail.com>
2021-08-06 15:38:49 +0300

    Added file CMakeLists.txt and modified file ex06_56.cpp

--- ex06_56.cpp
+++ ex06_56.cpp
@@ -5,8 +5,8 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-int tripleCallByValue(int);
-int tripleByReference(int&);
+void tripleCallByValue(int);
+void tripleByReference(int&);
 
 int main()
 {
@@ -16,23 +16,26 @@ int main()
    cin >> count;
    
    cout << "Initially count = " << count << endl;
+
+   tripleCallByValue(count)
    cout << "After passing count to a function by value;\n"
 	<< "count = " << count << endl << endl;
 
    cout << "Initially count = " << count << endl;
+
+   tripleByReference(count);
    cout << "After passing count to the function by reference;\n"
         << "count = " << count << endl << endl;
 
    return 0;
 }
 
-int tripleCallByValue(int count_v)
+void tripleCallByValue(int count_v)
 {
-   return count_v * 3;
-
+   coun_v = count_v * 3;
 }
 
-int tripleByReference(int& count_r)
+void tripleByReference(int& count_r)
 {
-   return count_r * 3;
+   count_r = count_r * 3;
 }
